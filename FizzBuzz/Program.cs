7a45fef207fd3a729e528606513d90caf0a58f04
Todd Cherveny <ToddChervenyjr@gmail.com>
2021-02-08 20:12:23 -0600

    initialcommit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            Console.ReadLine();
+            //Fizzbuzz(15);
+        }
+
+        public static void Fizzbuzz(int number)
+        {
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Fizzbuzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+        }
+    }
+}
